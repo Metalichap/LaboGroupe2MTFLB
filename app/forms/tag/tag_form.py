43e46042f752b68927fefdc4518d5e87cc49535a
Loïc <loic.glineur@student.uliege.be>
2026-08-13 14:44:41 +0200

    Add Tag Forms

---
+++ app/forms/tag/tag_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, TextAreaField
+from wtforms.validators import DataRequired, InputRequired, Length, NumberRange
+
+class TagForm(FlaskForm):
+
+    name = StringField('Nom du Tag', validators=[DataRequired(), Length(min=2, max=63)])
+    color = StringField('Couleur (format hex)', validators=[DataRequired(), Length(min=6, max=6)])
