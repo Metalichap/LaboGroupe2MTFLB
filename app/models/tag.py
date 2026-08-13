7ea58eccdffdee79f78132efff4fa86d15e0bdb7
Loïc <loic.glineur@student.uliege.be>
2026-08-13 14:44:41 +0200

    Add Tag Forms

--- app/models/tag.py
+++ app/models/tag.py
@@ -9,7 +9,7 @@ class Tag(BaseEntity, db.Model):
 
     tag_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tag_name = db.Column(db.String(63), unique=True, index=True, nullable=False)
-    tag_color = db.Column(db.String(8), nullable=False)
+    tag_color = db.Column(db.String(6), nullable=False)
 
     tickets = db.relationship('TicketTag', back_populates='tag')
 
