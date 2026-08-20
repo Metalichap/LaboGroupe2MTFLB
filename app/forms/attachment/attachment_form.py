00adbf30e84086f9f8ab5775aac303c5d913c9e5
ben <bengschor@gmail.com>
2026-08-20 15:39:51 +0200

    [dev_c - feature - attachment] adding attachment_mapper.py

--- app/forms/attachment/attachment_form.py
+++ app/forms/attachment/attachment_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_wtf.file import FilField, FileRequired, FileAllowed
 
 class AttachmentForm(FlaskForm):
     file = FileField("File", validators=[FileRequired(),
