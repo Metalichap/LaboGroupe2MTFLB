516b44bc92cd80be066f670205e9540a8f2d0b78
François <metalichap@hotmail.com>
2026-08-21 14:19:39 +0200

    [dev_b - feature - ticket] adding ticket forms

---
+++ app/forms/ticket/ticket_create_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, DateTimeField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class TicketCreateForm(FlaskForm):
+    ticket_title = StringField('ticket_title', validators=[DataRequired(), Length(max=63)])
+    ticket_description = TextAreaField('ticket_description', validators=[Optional()])
+    ticket_due_date = DateTimeField('ticket_due_date', format='%Y-%m-%dT%H:%M', validators=[Optional()])
+
+    category_id = SelectField('category_id', coerce=int, validators=[DataRequired()])
+    priority_id = SelectField('priority_id', coerce=int, validators=[DataRequired()])
