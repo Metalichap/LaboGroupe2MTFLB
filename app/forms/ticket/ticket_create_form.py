20af9ef1afd16e3de1da6c6218cec4e18e3854ea
François <metalichap@hotmail.com>
2026-08-21 15:36:12 +0200

    [dev_b - fix - ticket]  fix error on titcket status on ticket update form

--- app/forms/ticket/ticket_create_form.py
+++ app/forms/ticket/ticket_create_form.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length, Optional
 class TicketCreateForm(FlaskForm):
     ticket_title = StringField('ticket_title', validators=[DataRequired(), Length(max=63)])
     ticket_description = TextAreaField('ticket_description', validators=[Optional()])
-    ticket_due_date = DateTimeField('ticket_due_date', format='%Y-%m-%dT%H:%M', validators=[Optional()])
+    ticket_due_date = DateTimeField('ticket_due_date',  format='%Y-%m-%d' , validators=[Optional()])
 
     category_id = SelectField('category_id', coerce=int, validators=[DataRequired()])
     priority_id = SelectField('priority_id', coerce=int, validators=[DataRequired()])
