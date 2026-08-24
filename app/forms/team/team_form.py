4b84e1a803b6edec39ef6c8ce64120b6b4558029
Pehnny <theo.piron.pro@gmail.com>
2026-08-24 15:37:21 +0200

    [dev_a - feature - team] implémentation du service add team

---
+++ app/forms/team/team_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class TeamInsertForm(FlaskForm):
+    team_name = StringField('team_name', validators=[DataRequired(), Length(min=3, max=127)])
+    team_description = TextAreaField('team_description', validators=[Optional()])
