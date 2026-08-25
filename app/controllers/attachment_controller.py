c383b9769df0e1ef8aa03b8fda4faea503595d28
ben <bengschor@gmail.com>
2026-08-25 22:27:19 +0200

    [dev_c - feature - attachment] adding attachment download controller

--- app/controllers/attachment_controller.py
+++ app/controllers/attachment_controller.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, abort, send_from_directory
 
 from app import app
 from app.forms.attachment.attachment_form import AttachmentForm
@@ -6,6 +6,35 @@ from app.framework.decorators.auth_required import auth_required
 from app.services.auth_service import AuthService
 from app.framework.decorators.inject import inject
 from app.services.attachment_service import AttachmentService
+from pathlib import Path
+
+@app.post('/attachments/<int:attachment_id>/download')
+@auth_required()
+@inject
+def attachment_download(
+     attachment_id: int,
+     attachment_service: AttachmentService,
+     auth_service: AuthService):
+
+        current_user = auth_service.get_current_user()
+
+        if current_user is None:
+            abort(404)
+
+        attachment = attachment_service.find_for_download(
+        attachment_id=attachment_id,
+        current_user=current_user)
+
+        if attachment is None:
+            abort(404)
+
+        upload_path = Path(app.instance_path) / "attachments"
+
+        return send_from_directory(
+            upload_path,
+            attachment.attachment_path,
+            as_attachment=True,
+            download_name=attachment.attachment_filename)
 
 @app.post('/ticket/<int:ticket_id>/attachments')
 @auth_required()
